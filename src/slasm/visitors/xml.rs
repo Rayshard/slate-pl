c8fc9543f0e726957c4f61a9a924e34fc4d1c44f
Rayshard Thompson <rayshard.thompson@kensho.com>
2022-02-24 11:42:52 -0500

    lowercase xml

--- src/slasm/visitors/xml.rs
+++ src/slasm/visitors/xml.rs
@@ -7,17 +7,17 @@ use xml::writer::{EventWriter, XmlEvent};
 
 pub fn emit_instruction<W: Write>(instr: &Instruction, writer: &mut EventWriter<W>) {
     match instr {
-        Instruction::Noop => writer.write(XmlEvent::start_element("NOOP")).unwrap(),
-        Instruction::Or => writer.write(XmlEvent::start_element("OR")).unwrap(),
-        Instruction::And => writer.write(XmlEvent::start_element("AND")).unwrap(),
-        Instruction::Xor => writer.write(XmlEvent::start_element("XOR")).unwrap(),
-        Instruction::Not => writer.write(XmlEvent::start_element("NOT")).unwrap(),
-        Instruction::Shl => writer.write(XmlEvent::start_element("SHL")).unwrap(),
-        Instruction::Shr => writer.write(XmlEvent::start_element("SHR")).unwrap(),
-        Instruction::Ret => writer.write(XmlEvent::start_element("RET")).unwrap(),
+        Instruction::Noop => writer.write(XmlEvent::start_element("noop")).unwrap(),
+        Instruction::Or => writer.write(XmlEvent::start_element("or")).unwrap(),
+        Instruction::And => writer.write(XmlEvent::start_element("and")).unwrap(),
+        Instruction::Xor => writer.write(XmlEvent::start_element("xor")).unwrap(),
+        Instruction::Not => writer.write(XmlEvent::start_element("not")).unwrap(),
+        Instruction::Shl => writer.write(XmlEvent::start_element("shl")).unwrap(),
+        Instruction::Shr => writer.write(XmlEvent::start_element("shr")).unwrap(),
+        Instruction::Ret => writer.write(XmlEvent::start_element("ret")).unwrap(),
         Instruction::Push { data } => writer
             .write(
-                XmlEvent::start_element("PUSH").attr(
+                XmlEvent::start_element("push").attr(
                     "data",
                     &data
                         .iter()
@@ -28,108 +28,108 @@ pub fn emit_instruction<W: Write>(instr: &Instruction, writer: &mut EventWriter<
             )
             .unwrap(),
         Instruction::Pop { amt } => writer
-            .write(XmlEvent::start_element("POP").attr("amt", &amt.to_string()))
+            .write(XmlEvent::start_element("pop").attr("amt", &amt.to_string()))
             .unwrap(),
         Instruction::LoadLocal { name } => writer
-            .write(XmlEvent::start_element("LOAD_LOCAL").attr("name", name))
+            .write(XmlEvent::start_element("load_local").attr("name", name))
             .unwrap(),
         Instruction::StoreLocal { name } => writer
-            .write(XmlEvent::start_element("STORE_LOCAL").attr("name", name))
+            .write(XmlEvent::start_element("store_local").attr("name", name))
             .unwrap(),
         Instruction::LoadParam { name } => writer
-            .write(XmlEvent::start_element("LOAD_PARAM").attr("name", name))
+            .write(XmlEvent::start_element("load_param").attr("name", name))
             .unwrap(),
         Instruction::StoreParam { name } => writer
-            .write(XmlEvent::start_element("STORE_PARAM").attr("name", name))
+            .write(XmlEvent::start_element("store_param").attr("name", name))
             .unwrap(),
         Instruction::LoadGlobal { name } => writer
-            .write(XmlEvent::start_element("LOAD_GLOBAL").attr("name", name))
+            .write(XmlEvent::start_element("load_global").attr("name", name))
             .unwrap(),
         Instruction::StoreGlobal { name } => writer
-            .write(XmlEvent::start_element("STORE_GLOBAL").attr("name", name))
+            .write(XmlEvent::start_element("store_global").attr("name", name))
             .unwrap(),
         Instruction::LoadMem { offset } => writer
-            .write(XmlEvent::start_element("LOAD_MEM").attr("offset", &offset.to_string()))
+            .write(XmlEvent::start_element("load_mem").attr("offset", &offset.to_string()))
             .unwrap(),
         Instruction::StoreMem { offset } => writer
-            .write(XmlEvent::start_element("STORE_MEM").attr("offset", &offset.to_string()))
+            .write(XmlEvent::start_element("store_mem").attr("offset", &offset.to_string()))
             .unwrap(),
         Instruction::LoadLocalAddr { name } => writer
-            .write(XmlEvent::start_element("LOAD_LOCAL_ADDR").attr("name", name))
+            .write(XmlEvent::start_element("load_local_addr").attr("name", name))
             .unwrap(),
         Instruction::LoadParamAddr { name } => writer
-            .write(XmlEvent::start_element("LOAD_PARAM_ADDR").attr("name", name))
+            .write(XmlEvent::start_element("load_param_addr").attr("name", name))
             .unwrap(),
         Instruction::LoadGlobalAddr { name } => writer
-            .write(XmlEvent::start_element("LOAD_GLOBAL_ADDR").attr("name", name))
+            .write(XmlEvent::start_element("load_global_addr").attr("name", name))
             .unwrap(),
         Instruction::LoadFuncAddr { name } => writer
-            .write(XmlEvent::start_element("LOAD_FUNC_ADDR").attr("name", name))
+            .write(XmlEvent::start_element("load_func_addr").attr("name", name))
             .unwrap(),
         Instruction::Add { data_type } => writer
-            .write(XmlEvent::start_element("ADD").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("add").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Sub { data_type } => writer
-            .write(XmlEvent::start_element("SUB").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("sub").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Mul { data_type } => writer
-            .write(XmlEvent::start_element("MUL").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("mul").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Div { data_type } => writer
-            .write(XmlEvent::start_element("Div").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("div").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Mod { data_type } => writer
-            .write(XmlEvent::start_element("MOD").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("mod").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Inc { data_type } => writer
-            .write(XmlEvent::start_element("INC").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("inc").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Dec { data_type } => writer
-            .write(XmlEvent::start_element("DEC").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("dec").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Eq { data_type } => writer
-            .write(XmlEvent::start_element("EQ").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("eq").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Neq { data_type } => writer
-            .write(XmlEvent::start_element("NEQ").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("neq").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Gt { data_type } => writer
-            .write(XmlEvent::start_element("GT").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("gt").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Lt { data_type } => writer
-            .write(XmlEvent::start_element("LT").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("lt").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::GtEq { data_type } => writer
-            .write(XmlEvent::start_element("GTEQ").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("gteq").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::LtEq { data_type } => writer
-            .write(XmlEvent::start_element("LTEQ").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("lteq").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Neg { data_type } => writer
-            .write(XmlEvent::start_element("NEG").attr("data_type", &data_type.to_string()))
+            .write(XmlEvent::start_element("neg").attr("data_type", &data_type.to_string()))
             .unwrap(),
         Instruction::Convert { dt_from, dt_to } => writer
             .write(
-                XmlEvent::start_element("CONVERT")
+                XmlEvent::start_element("convert")
                     .attr("from", &dt_from.to_string())
                     .attr("to", &dt_to.to_string()),
             )
             .unwrap(),
         Instruction::Jump { target } => writer
-            .write(XmlEvent::start_element("JUMP").attr("target", target))
+            .write(XmlEvent::start_element("jump").attr("target", target))
             .unwrap(),
         Instruction::CondJump { target } => writer
-            .write(XmlEvent::start_element("COND_JUMP").attr("target", target))
+            .write(XmlEvent::start_element("cond_jump").attr("target", target))
             .unwrap(),
         Instruction::Call { target } => writer
-            .write(XmlEvent::start_element("Call").attr("target", target))
+            .write(XmlEvent::start_element("call").attr("target", target))
             .unwrap(),
         Instruction::IndirectCall {
             param_buffer_size,
             ret_buffer_size,
         } => writer
             .write(
-                XmlEvent::start_element("INDIRECT_CALL")
+                XmlEvent::start_element("indirect_call")
                     .attr("param_buffer_size", &param_buffer_size.to_string())
                     .attr("ret_buffer_size", &ret_buffer_size.to_string()),
             )
